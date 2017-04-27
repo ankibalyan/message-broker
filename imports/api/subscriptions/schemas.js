d6a3a59e60b1b64d513475a72408bac4e87f2b70
Ankit Kumar <ankit@ipapagari.com>
2017-04-27 23:24:47 +0530

    added subscriptions module

---
+++ imports/api/subscriptions/schemas.js
@@ -0,0 +1,45 @@
+import { Meteor } from 'meteor/meteor';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+
+export const Subscriptions = new Meteor.Collection('subscriptions');
+
+Subscriptions.allow({
+  insert: () => false,
+  update: () => false,
+  remove: () => false,
+});
+
+Subscriptions.deny({
+  insert: () => true,
+  update: () => true,
+  remove: () => true,
+});
+
+// Define the schema
+export const SubscriptionSchema = new SimpleSchema({
+  _id: {
+    type: String,
+    label: 'Id',
+    max: 200,
+  },
+  user: {
+    type: String,
+    label: 'Subscriber',
+    max: 200,
+    denyUpdate: true,
+  },
+  topicIds: {
+    type: [String],
+    label: 'Topic Id'
+  },
+  active: {
+    type: Boolean,
+    label: 'Subscriptions status',
+    default: true,
+    optional: true
+  },
+});
+
+Subscriptions.attachSchema(SubscriptionSchema);
+
+// TODO: ensure index
---
+++ imports/api/subscriptions/server/methods.js
@@ -0,0 +1,44 @@
+import { Meteor } from 'meteor/meteor';
+import { check, Match } from 'meteor/check';
+import { ValidatedMethod } from 'meteor/mdg:validated-method';
+
+import { Subscriptions, SubscriptionSchema } from '../schemas';
+
+export const subscribeToTopic = new ValidatedMethod({
+  name: 'subscribeToTopic',
+  validate(props) {
+    check(props, {
+      topicId: String,
+    });
+  },
+  run(props) {
+    const { topicId } = props;
+
+    if (!this.userId) {
+      throw new Meteor.Error('error-not-authorized', 'User need to login', { method: 'subscribeToTopic' });
+    }
+
+    // get user's subscription list if exist otherwise add new.
+    const subscriptions = Subscriptions.findOne({ user: this.userId })
+
+    if(subscriptions) {
+      // TODO check if user is already subscribed to this
+      try {
+        Subscriptions.update({ _id: subscriptions._id }, {$push: { topicIds } });
+      } catch (e) {
+        throw new Meteor.Error('error-500', 'Internal server error', { method: 'subscribeToTopic' });
+      }
+    } else {
+      const subRecord = {
+        user: this.userId,
+        topicIds,
+      };
+
+      try {
+        Subscriptions.insert(subRecord);
+      } catch (e) {
+        throw new Meteor.Error('error-500', 'Internal server error', { method: 'subscribeToTopic' });
+      }
+    }
+  }
+});
