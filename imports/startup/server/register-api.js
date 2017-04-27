7cd77344cef7fe877956736fe20284a181226cb6
Ankit Kumar <ankit@ipapagari.com>
2017-04-28 00:58:48 +0530

    registered publications and subscriptions api

--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -2,6 +2,11 @@
 
 import '/imports/api/topics/schemas';
 import '/imports/api/topics/server/methods';
+import '/imports/api/topics/server/publications';
 
 import '/imports/api/messages/schemas';
 import '/imports/api/messages/server/methods';
+import '/imports/api/messages/server/publications';
+
+import '/imports/api/subscriptions/schemas';
+import '/imports/api/subscriptions/server/methods';
